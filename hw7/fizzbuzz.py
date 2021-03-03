c076ba1fd3a5eac40171b04ffb10aba598f575ce
mmamel <mma0671@gmail.com>
2021-03-02 17:26:55 -0800

    Implemente fizzbuzz method where numbers from 1 to 100 are printed and when divisible by 3 its replaced with fizz, when by 5 it is replaced with buzz and when both it writes fizzbuzz

--- hw7/fizzbuzz.py
+++ hw7/fizzbuzz.py
@@ -1,2 +1,19 @@
 def fizzbuzz():
-	pass
+	list = []
+	for x in range(1,101):
+		if(x%3==0 and x%5==0):
+			list.append("FizzBuzz")
+		
+		elif(x%3 == 0 ):
+			list.append('Fizz')
+		
+		elif(x%5 == 0):
+			list.append("Buzz")
+		
+		else:
+			list.append(x)
+	return list
+	
+	
+
+print(fizzbuzz())
