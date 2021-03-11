994e1e31f899f4201d31560cc8c2fd4f4d712fae
mmamel <43307993+mmamel@users.noreply.github.com>
2021-03-11 09:14:01 -0800

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,14 @@
+"""
+Unit testing the calculator app
+"""
+
 import calculator
 
-class TestCalculator:
-	def test_add(self):
-		assert 6 == calculator.add(1, 5)
 
-	def test_subtract(self):
-		assert 2 == calculator.subtract(4,2)
+class TestCalculator:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
